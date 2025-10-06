e44ac19161c8047c70678525971d720d1ecff710
Polina <polina@Polina.>
2025-10-06 16:26:41 +0300

    Add docstrings and annotations

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from modules.rna_dna_tools import is_nucleic_acid, transcribe, reverse, complement, reverse_complement
 from modules.filter import is_in_bounds, is_qualified
+complement_RNA: dict
 complement_RNA = {
     "a": "u",
     "A": "U",
@@ -10,6 +11,7 @@ complement_RNA = {
     "c": "g",
     "C": "G",
 }
+complement_DNA: dict
 complement_DNA = {
     "a": "t",
     "A": "T",
@@ -20,10 +22,26 @@ complement_DNA = {
     "c": "g",
     "C": "G",
 }
+valid_RNA: dict
 valid_RNA = {"a", "u", "g", "c", "A", "U", "G", "C"}
+valid_DNA: dict
 valid_DNA = {"a", "t", "g", "c", "A", "T", "G", "C"}
 
-def run_dna_rna_tools(*args):
+def run_dna_rna_tools(*args: str) -> Union[str, list[str]]:
+    """
+    Processes rna or dna according to given order by last string in agrs
+    Arguments:
+    args[:-1]: str, sequences to process
+    agrs[-1]: str, what to do with a sequence
+    Returns str in case of a single sequence or list in case of 2 and more sequences
+    Possuble args[-1]:
+    is_nucleic_acid - check for correct nucleic acid
+    transcribe - transform DNA to RNA
+    reverse - write the sequence in the opposite direction
+    complement - write complement sequence
+    reverse_complement - combine reverse and complement transformations
+    Raises the error in case of incorrect sequences
+    """
     sequences = args[:-1]
     action = str(args[-1])
     available_functions = {
@@ -33,7 +51,6 @@ def run_dna_rna_tools(*args):
         "complement": complement,
         "reverse_complement": reverse_complement,
     }
-
     flag = is_nucleic_acid(sequences)
     if not flag:
         return flag
@@ -45,7 +62,25 @@ def run_dna_rna_tools(*args):
             return answer
         
 
-def filter_fastq(seqs, gc_bounds=(0, 100), length_bounds=(0, 2**32), quality_threshold=0):
+def filter_fastq(
+    seqs: dict[str: str], 
+    gc_bounds: Union[tuple[int, int], int, float] = (0, 100), 
+    length_bounds: Union[tuple[int, int], int, float] = (0, 2**32), 
+    quality_threshold: int = 0
+) -> dict[str, str]:
+    """
+    Filters fastq sequences by gives parameters
+    Arguments:
+    seqs: dict 
+    gc_bounds: tuples / int / float
+    length_bounds: tuples / int / float
+    quality_threshold: int
+    Returns dict
+    Raises the error in case reads are not correct nucleic acids
+    """
+    for key in seqs:
+        if not is_nucleic_acid(key):
+            return 'Error: reads are not nucleic acids'
     filtered_seqs = {}
     if isinstance(length_bounds, (int, float)):
         len_left_bound, len_right_bound = (0, length_bounds)
--- modules/filter.py
+++ modules/filter.py
@@ -1,4 +1,4 @@
-def is_in_bounds(sequence, range, *args):
+def is_in_bounds(sequence: str, range: Union[tuple[int, int], int, float], *args: str) -> bool:
     if isinstance(range, (int, float)):
         low_bound, upper_bound = 0, range
     else:
@@ -9,6 +9,6 @@ def is_in_bounds(sequence, range, *args):
     args_percentage = args_count/len(sequence)*100
     return low_bound <= args_percentage <= upper_bound
 
-def is_qualified(seq_quality, quality_threshold):
+def is_qualified(seq_quality: str, quality_threshold: int) -> bool:
     q_score = sum([ord(el)-33 for el in seq_quality])  
     return 10**(-q_score/10) >= quality_threshold
