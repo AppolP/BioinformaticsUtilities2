58ce673c7b986bead7013678bb21522746ab0596
Polina <polina@Polina.>
2025-10-13 23:07:16 +0300

    Add bio files processor functions and add some fixes

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from modules.rna_dna_tools import is_nucleic_acid, transcribe, reverse, complement, reverse_complement
 from modules.filter import is_in_bounds, is_qualified
-complement_RNA: dict
-complement_RNA = {
+from typing import Union
+
+complement_rna: dict
+complement_rna = {
     "a": "u",
     "A": "U",
     "u": "a",
@@ -11,8 +13,8 @@ complement_RNA = {
     "c": "g",
     "C": "G",
 }
-complement_DNA: dict
-complement_DNA = {
+complement_dna: dict
+complement_dna = {
     "a": "t",
     "A": "T",
     "t": "a",
@@ -22,14 +24,27 @@ complement_DNA = {
     "c": "g",
     "C": "G",
 }
-valid_RNA: dict
-valid_RNA = {"a", "u", "g", "c", "A", "U", "G", "C"}
-valid_DNA: dict
-valid_DNA = {"a", "t", "g", "c", "A", "T", "G", "C"}
+valid_rna: dict
+valid_rna = {"a", "u", "g", "c", "A", "U", "G", "C"}
+valid_dna: dict
+valid_dna = {"a", "t", "g", "c", "A", "T", "G", "C"}
+transcribed_dna: dict
+transcribed_dna = {
+    "a": "a",
+    "A": "A",
+    "t": "u",
+    "T": "U",
+    "g": "g",
+    "G": "G",
+    "c": "c",
+    "C": "C",
+}
 
 def run_dna_rna_tools(*args: str) -> Union[str, list[str]]:
+    
     """
     Processes rna or dna according to given order by last string in agrs
+    
     Arguments:
     args[:-1]: str, sequences to process
     agrs[-1]: str, what to do with a sequence
@@ -40,10 +55,11 @@ def run_dna_rna_tools(*args: str) -> Union[str, list[str]]:
     reverse - write the sequence in the opposite direction
     complement - write complement sequence
     reverse_complement - combine reverse and complement transformations
-    Raises the error in case of incorrect sequences
+    
+    Raises an error in case of incorrect sequences
     """
-    sequences = args[:-1]
-    action = str(args[-1])
+    
+    *sequences, action= args
     available_functions = {
         "is_nucleic_acid": is_nucleic_acid,
         "transcribe": transcribe,
@@ -54,12 +70,10 @@ def run_dna_rna_tools(*args: str) -> Union[str, list[str]]:
     flag = is_nucleic_acid(sequences)
     if not flag:
         return flag
-    else:
-        answer = available_functions[action](sequences)
-        if len(answer) == 1:
-            return str(answer[0])
-        else:
-            return answer
+    answer = available_functions[action](sequences)
+    if len(answer) == 1:
+        return str(answer[0])
+    return answer
         
 
 def filter_fastq(
@@ -68,18 +82,22 @@ def filter_fastq(
     length_bounds: Union[tuple[int, int], int, float] = (0, 2**32), 
     quality_threshold: int = 0,
     output_fastq: str = 'filtered/filtered_fastq.fastq'
-) -> dict[str, str]:
+) -> None:                        
+    
     """
     Read and filter fastq sequences in a user-specified dierectory by given parameters
+    
     Arguments:
     input_fastq: str 
     gc_bounds: tuples / int / float
     length_bounds: tuples / int / float
     quality_threshold: int
     output_fastq: str
+    
     Returns filtered fastq sequences in a user-specified directory. Saves results in the current directory by default 
     Raises the error in case reads are not correct nucleic acids
     """
+    
     if not os.path.exists('filtered'):
         os.makedirs('filtered')
     if os.path.exists('filtered/output_fastq'):
