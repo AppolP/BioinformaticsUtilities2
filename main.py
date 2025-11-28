831374b4c1cd37a1218a099bea3efe0a19f5762e
Polina <polina@Polina.>
2025-11-29 01:54:14 +0300

    not kidding, final changes

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from modules.rna_dna_tools import is_nucleic_acid, transcribe, reverse, complement, reverse_complement
+from modules.rna_dna_tools import (
+    is_nucleic_acid,
+    transcribe,
+    reverse,
+    complement,
+    reverse_complement,
+)
 from modules.filter import is_in_bounds, is_qualified
 from typing import Union
 import os
@@ -41,11 +47,11 @@ transcribed_dna = {
     "C": "C",
 }
 
+
 def run_dna_rna_tools(*args: str) -> Union[str, list[str]]:
-    
     """
     Processes rna or dna according to given order by last string in agrs
-    
+
     Arguments:
     args[:-1]: str, sequences to process
     agrs[-1]: str, what to do with a sequence
@@ -56,11 +62,11 @@ def run_dna_rna_tools(*args: str) -> Union[str, list[str]]:
     reverse - write the sequence in the opposite direction
     complement - write complement sequence
     reverse_complement - combine reverse and complement transformations
-    
+
     Raises an error in case of incorrect sequences
     """
-    
-    *sequences, action= args
+
+    *sequences, action = args
     available_functions = {
         "is_nucleic_acid": is_nucleic_acid,
         "transcribe": transcribe,
@@ -75,71 +81,64 @@ def run_dna_rna_tools(*args: str) -> Union[str, list[str]]:
     if len(answer) == 1:
         return str(answer[0])
     return answer
-        
+
 
 def filter_fastq(
-    input_fastq: str, 
-    gc_bounds: Union[tuple[int, int], int, float] = (0, 100), 
-    length_bounds: Union[tuple[int, int], int, float] = (0, 2**32), 
+    input_fastq: str,
+    gc_bounds: Union[tuple[int, int], int, float] = (0, 100),
+    length_bounds: Union[tuple[int, int], int, float] = (0, 2**32),
     quality_threshold: int = 0,
-    output_fastq: str = 'filtered/filtered_fastq.fastq'
-) -> None:                        
-    
+    output_fastq: str = "filtered/filtered_fastq.fastq",
+) -> None:
     """
     Read and filter fastq sequences in a user-specified dierectory by given parameters
-    
+
     Arguments:
-    input_fastq: str 
+    input_fastq: str
     gc_bounds: tuples / int / float
     length_bounds: tuples / int / float
     quality_threshold: int
     output_fastq: str
-    
-    Returns filtered fastq sequences in a user-specified directory. Saves results in the current directory by default 
+
+    Returns filtered fastq sequences in a user-specified directory. Saves results in the current directory by default
     Raises the error in case reads are not correct nucleic acids
     """
-    
-    if not os.path.exists('filtered'):
-        os.makedirs('filtered')
-    if os.path.exists('filtered/output_fastq'):
-        return 'File already exists!'
-    
+
+    if not os.path.exists("filtered"):
+        os.makedirs("filtered")
+    if os.path.exists("filtered/output_fastq"):
+        return "File already exists!"
+
     if isinstance(length_bounds, (int, float)):
         len_left_bound, len_right_bound = (0, length_bounds)
     else:
-        len_left_bound, len_right_bound = length_bounds    
-        
-    with open(input_fastq, 'r') as raw_fastq, open(output_fastq, 'w'):
-        output_fastq.write(f"{input_fastq = }{'\n'}")
-        
-        skip_comma = True
-        while raw_fastq.readline().strip() != '':
-            name_line = raw_fastq.readline().strip(':')[0]
+        len_left_bound, len_right_bound = length_bounds
+
+    with open(input_fastq, "r") as raw_fastq, open(output_fastq, "w") as output_fastq:
+        while True:
+            name_line = raw_fastq.readline().strip()
+            if not name_line:
+                break
+
             sequence = raw_fastq.readline().strip()
-            comment = raw_fastq.readline()
+            plus_line = raw_fastq.readline().strip()
             quality = raw_fastq.readline().strip()
-            
+
+            if not name_line.startswith("@"):
+                continue
             if not is_nucleic_acid(sequence):
-                return 'Error: reads are not nucleic acids'
-            name = name_line[1:] if name_line.startswith('@') else name_line
-            if (is_in_bounds(sequence, gc_bounds, 'G', 'C') and 
-                is_qualified(quality, quality_threshold) and 
-                len_left_bound <= len(sequence) <= len_right_bound):
-                if not skip_comma:
-                    output_fastq.write(',\n')
-                output_fastq.write(f"'{name}': ('{sequence}', '{quality}')")
-                skip_comma = False
-                
-        output_fastq.write("/n}}")
-       
-    # for key in seqs:
-    #     if not is_nucleic_acid(key):
-    #         return 'Error: reads are not nucleic acids'
-    # filtered_seqs = {}
-    
-    # for seq in seqs:
-    #     if is_in_bounds(seqs[seq][0], gc_bounds, 'G', 'C') and is_qualified(seqs[seq][1], quality_threshold) and len_left_bound <= len(seqs[seq][0]) <= len_right_bound:
-    #         filtered_seqs[seq] = seqs[seq]
-    # return filtered_seqs
-          
-    
+                print("Error: reads are not nucleic acids")
+                return None
+
+            name = name_line[1:]
+
+            if (
+                is_in_bounds(sequence, gc_bounds, "G", "C")
+                and (is_qualified(quality, quality_threshold))
+                and (len_left_bound <= len(sequence) <= len_right_bound)
+            ):
+
+                output_fastq.write(f"{name_line}\n")
+                output_fastq.write(f"{sequence}\n")
+                output_fastq.write(f"{plus_line}\n")
+                output_fastq.write(f"{quality}\n")
